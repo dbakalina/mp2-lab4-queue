e9167caca74142a928a5b709e20e51d91cec1495
dbakalina <darya.bakalina@gmail.com>
2020-12-13 13:54:56 +0300

    Test

--- Queue/Test_queue/Test_queue.cpp
+++ Queue/Test_queue/Test_queue.cpp
@@ -13,26 +13,26 @@ TEST(Queue, cant_create_queue_with_negative_MaxSize)
 
 TEST(Queue, can_get_size)
 {
-    TQueue<int> q(5);
-    q.Push(5);
+    TQueue<int> q(7);
+    q.Push(7);
     ASSERT_NO_THROW(q.GetSize());
     EXPECT_EQ(1, q.GetSize());
 }
 
-TEST(Queue, can_copy_the_queue)
-{
-    TQueue<int> q(5);
-    q.Push(5);
-    ASSERT_NO_THROW(TQueue<int> copy(q));
-}
-
 TEST(Queue, the_copied_queue_is_the_same_as_the_original_one)
 {
-    TQueue<int> q(5);
-    q.Push(5);
+    TQueue<int> q(7);
+    q.Push(7);
     TQueue<int> copy(q);
     EXPECT_EQ(q.GetSize(), copy.GetSize());
     EXPECT_EQ(q.GetHead(), copy.GetHead());
+}
+
+TEST(Queue, can_copy_the_queue)
+{
+    TQueue<int> q(7);
+    q.Push(7);
+    ASSERT_NO_THROW(TQueue<int> copy(q));
 }
 
 TEST(Queue, can_assign_queue_of_the_same_data_types_and_MaxSize)
@@ -90,7 +90,7 @@ TEST(Queue, comparing_the_same_queue_gives_the_truth)
     EXPECT_EQ(true, q1 == q2);
 }
 
-TEST(Queue, comparing_different_queue_gives_the_lie)
+TEST(Queue, comparing_different_queue_gives_the_false)
 {
     TQueue<int> q1(5);
     TQueue<int> q2(5);
@@ -104,22 +104,20 @@ TEST(Queue, can_check_the_queue_for_emptiness)
     TQueue<int> q(5);
     ASSERT_NO_THROW(q.Empty());
 }
-TEST(Queue, initially_the_queue_is_empty)
-{
-    TQueue<int> q(5);
-    EXPECT_EQ(true, q.Empty());
-}
+
 TEST(Queue, a_queue_with_a_size_greater_than_zero_is_not_empty)
 {
     TQueue<int> q(5);
     q.Push(5);
     EXPECT_EQ(false, q.Empty());
 }
+
 TEST(Queue, can_check_the_queue_for_completeness)
 {
     TQueue<int> q(5);
     ASSERT_NO_THROW(q.Full());
 }
+
 TEST(Queue, if_the_queue_size_is_equal_to_the_MaxSize_then_the_stack_is_full)
 {
     int max_size = 2;
@@ -129,23 +127,28 @@ TEST(Queue, if_the_queue_size_is_equal_to_the_MaxSize_then_the_stack_is_full)
     EXPECT_EQ(q.GetSize(), max_size);
     EXPECT_EQ(true, q.Full());
 }
+
 TEST(Queue, can_add_an_item_to_an_incomplete_queue)
 {
     TQueue<int> q(5);
     ASSERT_NO_THROW(q.Push(5));
 }
+
 TEST(Queue, cant_add_an_item_to_the_full_queue)
 {
     TQueue<int> q(1);
     q.Push(5);
     ASSERT_ANY_THROW(q.Push(5));
 }
+
+
 TEST(Queue, can_see_what_is_at_the_top_of_a_non_empty_queue)
 {
     TQueue<int> q(5);
     q.Push(5);
     ASSERT_NO_THROW(q.SeeHead());
 }
+
 TEST(Queue, viewing_the_top_of_the_stack_does_not_change_the_state_of_the_queue)
 {
     TQueue<int> q(5);
@@ -155,30 +158,37 @@ TEST(Queue, viewing_the_top_of_the_stack_does_not_change_the_state_of_the_queue)
     EXPECT_EQ(size, q.GetSize());
     EXPECT_EQ(a, q.SeeHead());
 }
+
 TEST(Queue, cant_look_at_the_top_of_an_empty_queue)
 {
     TQueue <int> q(5);
     ASSERT_ANY_THROW(q.GetHead());
 }
+
 TEST(Queue, can_take_an_element_from_a_non_empty_non_empty_queue)
 {
     TQueue<int> q(5);
     q.Push(5);
     ASSERT_NO_THROW(q.GetHead());
 }
-TEST(Queue, taking_an_element_from_the_queue_reduces_its_size_by_one)
-{
-    TQueue<int> q(5);
-    int a = 2, b = 3;
-    q.Push(a);
-    q.Push(b);
-    int size = q.GetSize();
-    int elem = q.GetHead();
-    EXPECT_EQ(elem, a);
-    EXPECT_EQ(size - 1, q.GetSize());
-}
-TEST(Queue, cant_take_an_element_from_an_empty_stack)
+
+TEST(Queue, cant_take_an_element_from_an_empty_queue)
 {
     TQueue<int> q(5);
     ASSERT_ANY_THROW(q.GetHead());
+}
+
+TEST(TQueue, can_not_put_elem_in_full_queue)
+{
+    TQueue<int> q(2);
+    q.Push(1);
+    q.Push(2);
+    ASSERT_ANY_THROW(q.Push(3));
+}
+
+TEST(TQueue, can_correcttly_get_elem_from_the_queue)
+{
+    TQueue<int> q(4);
+    q.Push(2);
+    EXPECT_EQ(q.GetHead(), 2);
 }
